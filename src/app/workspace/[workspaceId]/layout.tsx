1ea931c765fe980cfc98dc04b98c0e9835c8a96b
lsj2015-hub <lsj2015@hotmail.com>
2024-09-03 18:19:32 +0800

    Toolbar component

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,16 @@
+import Toolbar from './toolbar';
+
+interface WorkspaceLayoutProps {
+  children: React.ReactNode;
+}
+
+const WorkspaceLayout = ({ children }: WorkspaceLayoutProps) => {
+  return (
+    <div className="h-full">
+      <Toolbar />
+      {children}
+    </div>
+  );
+};
+
+export default WorkspaceLayout;
