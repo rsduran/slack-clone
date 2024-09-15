253e8555dda019048b72293f350c7c76df42540d
lsj2015-hub <lsj2015@hotmail.com>
2024-09-15 14:41:01 +0800

    Channel Hero

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import { differenceInMinutes, format, isToday, isYesterday } from 'date-fns';
 
 import { GetMessagesReturnType } from '@/features/messages/api/use-get-messages';
+import { useCurrentMember } from '@/features/members/api/use-current-member';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
 import { Message } from './message';
+import { ChannelHero } from './channel-hero';
+import { Id } from '../../convex/_generated/dataModel';
 
 const TIME_THRESHOLD = 5;
 
@@ -35,6 +41,12 @@ export const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }: MessageListProps) => {
+  const [editingId, setEditingId] = useState<Id<'messages'> | null>(null);
+
+  const workspaceId = useWorkspaceId();
+
+  const { data: currentMember } = useCurrentMember({ workspaceId });
+
   const groupedMessages = data?.reduce(
     (groups, message) => {
       const date = new Date(message._creationTime);
@@ -76,16 +88,16 @@ export const MessageList = ({
                 memberId={message.memberId}
                 authorImage={message.user.image}
                 authorName={message.user.name}
-                isAuthor={false}
+                isAuthor={message.memberId === currentMember?._id}
                 reactions={message.reactions}
                 body={message.body}
                 image={message.image}
                 updatedAt={message.updatedAt}
                 createdAt={message._creationTime}
-                isEditing={false}
-                setEditingId={() => {}}
+                isEditing={editingId === message._id}
+                setEditingId={setEditingId}
                 isCompact={isCompact}
-                hideThreadButton={false}
+                hideThreadButton={variant === 'thread'}
                 threadCount={message.threadCount}
                 threadImage={message.threadImage}
                 threadTimestamp={message.threadTimestamp}
@@ -94,6 +106,9 @@ export const MessageList = ({
           })}
         </div>
       ))}
+      {variant === 'channel' && channelName && channelCreationTime && (
+        <ChannelHero name={channelName} creationTime={channelCreationTime} />
+      )}
     </div>
   );
 };
--- src/components/message.tsx
+++ src/components/message.tsx
@@ -34,7 +34,7 @@ interface MessageProps {
 }
 
 const formatFullTime = (date: Date) => {
-  return `${isToday(date) ? 'Today' : isYesterday(date) ? 'Yesterday' : format(date, 'MMM d, YYYY')} at ${format(date, 'h:mm:ss a')}`;
+  return `${isToday(date) ? 'Today' : isYesterday(date) ? 'Yesterday' : format(date, 'MMM d, yyyy')} at ${format(date, 'h:mm:ss a')}`;
 };
 
 export const Message = ({
